f054a2db56cde79004043ad7ce304b65d8f0a22b
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-02 20:10:07 +0000

    7th done

--- exercises/7.js
+++ exercises/7.js
@@ -1,1 +1,10 @@
 // Extend Constructors to Receive Arguments
+function Dog(name, color) {
+  this.name = name;
+  this.color = color;
+  this.numLegs = 4;
+}
+
+
+
+let terrier = new Dog("dog", "black");
