52b4fa74527fead401e9223531972fac93137452
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-04 16:53:28 +0000

    20th done

--- exercises/20.js
+++ exercises/20.js
@@ -1,3 +1,18 @@
 // Reset an Inherited Constructor Property
 
 
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// Only change code below this line
+
+Dog.prototype.constructor = Dog;
+Bird.prototype.constructor = Bird;
+
+
+let duck = new Bird();
+let beagle = new Dog();
